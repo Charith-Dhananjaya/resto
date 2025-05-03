91f5db0b32fc01a6fbcbec8496778f62738518bf
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2025-05-03 21:46:55 +0530

    finalized

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '28b3755e-6d7b-46fc-8448-6f6d3421326b';
+export const MODAL_CLOSE_SEC = 2.5;
